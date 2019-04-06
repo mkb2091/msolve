ed9d7c2f224094855b81b7afaf9175b11984ddf9
Alex Williams <hughaw64@gmail.com>
2019-04-06 11:54:58 +0100

    Added apply_numbers

--- src/techniques.rs
+++ src/techniques.rs
@@ -1,22 +1,71 @@
 use crate::consts;
 
+pub fn apply_number(sudoku: &mut [u16; 81], square: usize) {
+    assert!(square < 81);
+    let value = sudoku[square] & consts::SUDOKU_VALUES_TOTAL;
+    let other_techniques = sudoku[square] & (consts::SUDOKU_TECHNIQUES_TOTAL);
+    let not_value = consts::SUDOKU_MAX - value;
+    let row_start = square / 9 * 9;
+    sudoku[row_start + 8] = (sudoku[row_start + 8] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 8] & value) / value));
+    sudoku[row_start + 7] = (sudoku[row_start + 7] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 7] & value) / value));
+    sudoku[row_start + 6] = (sudoku[row_start + 6] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 6] & value) / value));
+    sudoku[row_start + 5] = (sudoku[row_start + 5] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 5] & value) / value));
+    sudoku[row_start + 4] = (sudoku[row_start + 4] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 4] & value) / value));
+    sudoku[row_start + 3] = (sudoku[row_start + 3] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 3] & value) / value));
+    sudoku[row_start + 2] = (sudoku[row_start + 2] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 2] & value) / value));
+    sudoku[row_start + 1] = (sudoku[row_start + 1] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start + 1] & value) / value));
+    sudoku[row_start] = (sudoku[row_start] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[row_start] & value) / value));
+
+    let column_start = square % 9;
+    sudoku[column_start + 72] = (sudoku[column_start + 72] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 72] & value) / value));
+    sudoku[column_start + 63] = (sudoku[column_start + 63] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 63] & value) / value));
+    sudoku[column_start + 54] = (sudoku[column_start + 54] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 54] & value) / value));
+    sudoku[column_start + 45] = (sudoku[column_start + 45] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 45] & value) / value));
+    sudoku[column_start + 36] = (sudoku[column_start + 36] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 36] & value) / value));
+    sudoku[column_start + 27] = (sudoku[column_start + 27] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 27] & value) / value));
+    sudoku[column_start + 18] = (sudoku[column_start + 18] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 18] & value) / value));
+    sudoku[column_start + 9] = (sudoku[column_start + 9] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start + 9] & value) / value));
+    sudoku[column_start] = (sudoku[column_start] & not_value)
+        | (consts::SUDOKU_TECHNIQUES_TOTAL * ((sudoku[column_start] & value) / value));
+
+    let (_, _, boxes) = consts::PRECOMPUTED_INDEXES[square];
+    for cbox in boxes.iter() {
+        if sudoku[*cbox as usize] & value != 0 {
+            sudoku[*cbox as usize] =
+                (sudoku[*cbox as usize] ^ (value)) | consts::SUDOKU_TECHNIQUES_TOTAL;
+        }
+    }
+    sudoku[square] = value | other_techniques;
+}
+
+#[inline(never)]
 pub fn naked_n(sudoku: &mut [u16; 81]) -> bool {
     let mut changed: bool = false;
     for square in 0..81 {
-        let value = sudoku[square] & consts::SUDOKU_VALUES_TOTAL;
         if sudoku[square] & 512 == 512 {
+            let value = sudoku[square] & consts::SUDOKU_VALUES_TOTAL;
             let (rows, columns, boxes) = consts::PRECOMPUTED_INDEXES[square];
             // Naked singles
             if consts::OPTION_COUNT_CACHE[value as usize] == 1 {
+                apply_number(sudoku, square);
                 changed = true;
-                for house in [rows, columns, boxes].iter() {
-                    for pos in house.iter() {
-                        if sudoku[*pos as usize] & value != 0 {
-                            sudoku[*pos as usize] =
-                                (sudoku[*pos as usize] ^ (value)) | consts::SUDOKU_TECHNIQUES_TOTAL;
-                        }
-                    }
-                }
             // Naked Pairs
             } else if consts::OPTION_COUNT_CACHE[value as usize] == 2 {
                 for house in [rows, columns, boxes].iter() {
